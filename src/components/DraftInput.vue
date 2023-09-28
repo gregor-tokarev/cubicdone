a46bce15e5f46eee4212d356854cd2cee76a1d8b
gregor <e@tokarev.work>
2023-09-28 17:59:23 +0300

    Add autofocus in inbox page

--- src/components/DraftInput.vue
+++ src/components/DraftInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Icon from "./Icon.vue";
+import {onMounted, ref} from "vue";
 
 defineProps<{
   placeholder: string,
@@ -17,12 +18,19 @@ function onUpdate(event: Event) {
 
   emit("update:modelValue", target.value)
 }
+
+const inputField = ref(null)
+onMounted(() => {
+  const el = inputField.value as unknown as HTMLElement
+  el.focus()
+})
 </script>
 
 <template>
   <div class="flex items-center bg-gray-400 rounded-lg px-2.5 py-1">
     <Icon name="plus" class="text-gray-350 mr-1.5"></Icon>
     <input type="text" :value="modelValue" :placeholder="placeholder"
+           ref="inputField"
            @input="onUpdate"
            @keyup.enter="emit('enter')"
            class="bg-transparent placeholder-gray-350 text-base w-full border-0 outline-0">
