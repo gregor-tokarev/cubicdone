a91b302b340e6fed77c8fb8f3eb3b3b7f417ba44
gregor <e@tokarev.work>
2023-10-20 10:01:09 +0300

    Add workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+    }),
+  ],
 });
