b110f9487ee1f5307b90341897f100f70b52d18a
gregor <e@tokarev.work>
2023-12-14 09:13:31 +0300

    Add future checkbox

--- src/components/cards/DraftRow.vue
+++ src/components/cards/DraftRow.vue
@@ -45,27 +45,31 @@ function onKeydown(evt: KeyboardEvent) {
 
 <template>
   <div
-    class="flex cursor-grab items-baseline border-b border-gray-400 p-2.5 active:cursor-grabbing"
+    class="flex cursor-grab items-center border-b border-gray-400 p-2.5 active:cursor-grabbing"
   >
-    <!--    Title-->
-    <div
-      v-if="mode === 'edit'"
-      contenteditable="true"
-      @input="onEditDraft($event)"
-      @keydown="onKeydown($event)"
-      @blur="mode = 'view'"
-      ref="editEl"
-      class="cursor-text text-base text-black outline-0"
-    >
-      {{ draft.title }}
-    </div>
-    <Markdown
-      v-else-if="mode === 'view'"
-      @click="mode = 'edit'"
-      :model-value="draft.title"
-    ></Markdown>
-    <div class="ml-2.5 text-xs text-gray-300">
-      {{ dayjs(draft.dateCreated).format("D MMM, HH:mm") }}
+    <!--    checkbox-->
+    <div class="mr-1.5 h-4 w-4 cursor-pointer"></div>
+    <div class="flex items-baseline">
+      <!--    Title-->
+      <div
+        v-if="mode === 'edit'"
+        contenteditable="true"
+        @input="onEditDraft($event)"
+        @keydown="onKeydown($event)"
+        @blur="mode = 'view'"
+        ref="editEl"
+        class="cursor-text text-base text-black outline-0"
+      >
+        {{ draft.title }}
+      </div>
+      <Markdown
+        v-else-if="mode === 'view'"
+        @click="mode = 'edit'"
+        :model-value="draft.title"
+      ></Markdown>
+      <div class="ml-2.5 text-xs text-gray-300">
+        {{ dayjs(draft.dateCreated).format("D MMM, HH:mm") }}
+      </div>
     </div>
     <ProjectTag
       class="ml-auto"
