68a338542d709f79db06e9ee9a079d7cd930217d
gregor <e@tokarev.work>
2024-01-30 09:03:12 +0300

    Add vercel analytics package

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import App from "./App.vue";
 import { router } from "./router";
 import { createPinia } from "pinia";
 import { hint } from "./directives/hint.ts";
+import { inject } from "@vercel/analytics";
 
 const pinia = createPinia();
+
+inject();
 
 createApp(App).directive("hint", hint).use(router).use(pinia).mount("#app");
