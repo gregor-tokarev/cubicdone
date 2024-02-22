1e0ac21f3d7a0e27b0e761a548b205b5043c0547
gregor <e@tokarev.work>
2024-02-22 16:32:58 +0300

    Add AuthButton.vue

---
+++ src/components/UI/AuthButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="min-w-[520px] rounded-md border border-gray-400 py-4 text-[20px] text-gray-800 transition-colors hover:bg-gray-150"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped></style>
