2f3777cf6fb0869748782607467a7054ebc46ea5
gregor <e@tokarev.work>
2024-02-26 09:13:54 +0300

    Add guard authorization

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAuth } from "vue-clerk";
+import { watch } from "vue";
+import { useRouter } from "vue-router";
+
+const { isSignedIn, isLoaded } = useAuth();
+const router = useRouter();
+
+watch(isLoaded, () => {
+  if (!isLoaded.value) return;
+
+  if (!isSignedIn.value) {
+    router.replace("/auth");
+  }
+});
+</script>
 
 <template>
-  <router-view></router-view>
+  <router-view v-if="isLoaded"></router-view>
 </template>
 
 <style scoped></style>
