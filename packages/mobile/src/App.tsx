32e2ce6484f705349d200ae8122b773b90ed4243
gregor <e@tokarev.work>
2024-04-17 10:25:38 +0300

    Fix nativewind

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,24 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 
 export default function App() {
   return <Index></Index>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
 function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-400">
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
--- packages/mobile/tailwind.config.js
+++ packages/mobile/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.tsx"],
+  content: ["./src/App.tsx", "./src/**/*.tsx"],
   theme: {
     extend: {},
   },
