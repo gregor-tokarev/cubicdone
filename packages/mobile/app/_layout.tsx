315dd3eee255e6bdabeeecc8c7fc4636d76e8e47
gregor <e@tokarev.work>
2024-04-19 12:18:51 +0300

    Add fonts

--- packages/mobile/app/_layout.tsx
+++ packages/mobile/app/_layout.tsx
@@ -1,13 +1,24 @@
 import { SafeAreaView, Text, View } from "react-native";
 import { Slot } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useFonts } from "expo-font";
 
 export default function Root() {
+  const [fontLoaded, _fontError] = useFonts({
+    Poppins300: require("../assets/font/poppins-v21-latin-300.ttf"),
+    Poppins: require("../assets/font/poppins-v21-latin-regular.ttf"),
+    Poppins500: require("../assets/font/poppins-v21-latin-500.ttf"),
+    Poppins600: require("../assets/font/poppins-v21-latin-600.ttf"),
+    Poppins700: require("../assets/font/poppins-v21-latin-700.ttf"),
+  });
+
+  if (!fontLoaded) return <Text>Loading...</Text>;
+
   return (
     <GestureHandlerRootView className="flex-1">
       <SafeAreaView className="flex-1">
-        <View className="h-[35px] border-b border-gray-200">
-          <Text className="text-center text-xl">TODO</Text>
+        <View className="h-[35px] justify-center border-b border-gray-200">
+          <Text className="text-center font-[Poppins300] text-xl">TODO</Text>
         </View>
         <Slot></Slot>
       </SafeAreaView>
--- packages/mobile/app/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Settings() {
-  return (
-    <View>
-      <Text>Settings</Text>
-    </View>
-  );
-}
