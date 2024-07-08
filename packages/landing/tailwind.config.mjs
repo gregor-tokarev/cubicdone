5bcc4661f214d1378294039d3ce41a99d5a34bd6
gregor <e@tokarev.work>
2024-07-08 13:55:04 +0300

    Create landing

--- packages/landing/tailwind.config.mjs
+++ packages/landing/tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [
+    {pattern: /col-start-[0-9]/},
+  ],
   theme: {
     extend: {
       colors: {
