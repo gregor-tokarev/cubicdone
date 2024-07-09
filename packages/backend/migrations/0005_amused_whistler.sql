f08967252708dd90cbfe5ca178fa6d434d361a34
gregor <e@tokarev.work>
2024-07-09 08:36:55 +0300

    Fixate turbo version

---
+++ packages/backend/migrations/0005_amused_whistler.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "drafts" ALTER COLUMN "dateCreated" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "drafts" ALTER COLUMN "dateUpdated" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "drafts" ALTER COLUMN "order" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "title" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "color" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "project" ALTER COLUMN "order" SET NOT NULL;
