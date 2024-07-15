6241808f5fb1b53c2a65521cfd71a2c8f610df5e
gregor <e@tokarev.work>
2024-07-15 13:13:03 +0300

    Add vercel analytics

--- packages/landing/astro.config.mjs
+++ packages/landing/astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+    integrations: [tailwind(), sitemap()],
+    output: "static",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true
+        }
+    })
 });
