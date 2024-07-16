ccc065b790d82b78ab839c30191aca0a13f23885
gregor <e@tokarev.work>
2024-07-16 08:19:20 +0300

    Change db origin

--- packages/landing/astro.config.mjs
+++ packages/landing/astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
