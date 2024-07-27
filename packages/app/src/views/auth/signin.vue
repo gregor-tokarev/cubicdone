ffb7c7690c4dffe8b4ca86f8ae33c9e17066be3d
gregortokarev <gregortokarev@gregortokarevs-MacBook-Pro.local>
2024-07-27 09:54:27 +0300

    translate auth page

--- packages/app/src/views/auth/signin.vue
+++ packages/app/src/views/auth/signin.vue
@@ -2,39 +2,44 @@
 import AuthButton from "@components/UI/AuthButton.vue";
 import { ref } from "vue";
 import Icon from "../../components/Icon.vue";
+import { useI18n } from "vue-i18n";
 
 const opening = ref(false);
+const { t } = useI18n({
+    messages: {
+        en: {
+            title: "Signin into cubicdone",
+            buttonText: "Continue with {provider}"
+        },
+        ru: {
+            title: "Вход в cubicdone",
+            buttonText: "Продолжить с {provider}"
+        }
+    }
+})
 
 async function onSignin(strategy: "google" | "linear") {
-  opening.value = true;
-  location.href = `https://api.cubicdone.com/oauth/${strategy}`;
+    opening.value = true;
+    location.href = `https://api.cubicdone.com/oauth/${strategy}`;
 }
 </script>
 
 <template>
-  <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-    <h1 class="mb-[30px] text-center text-[32px] text-gray-800">
-      Sign In to personal_todo
-    </h1>
-    <div class="space-y-4">
-      <AuthButton @click="onSignin('google')" :disabled="opening">
-        <Icon
-          v-if="opening"
-          name="sync"
-          class="animate-spin text-gray-800"
-        ></Icon>
-        <Icon v-else :size="24" name="google"></Icon>
-        <span>Continue with google</span>
-      </AuthButton>
-      <AuthButton @click="onSignin('linear')" :disabled="opening">
-        <Icon
-          v-if="opening"
-          name="sync"
-          class="animate-spin text-gray-800"
-        ></Icon>
-        <Icon v-else :size="24" name="linear"></Icon>
-        <span>Continue with linear</span>
-      </AuthButton>
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 class="mb-[30px] text-center text-[32px] text-gray-800">
+            {{ t("title") }}
+        </h1>
+        <div class="space-y-4">
+            <AuthButton @click="onSignin('google')" :disabled="opening">
+                <Icon v-if="opening" name="sync" class="animate-spin text-gray-800"></Icon>
+                <Icon v-else :size="24" name="google"></Icon>
+                <span>{{ t("buttonText", { provider: "google" }) }}</span>
+            </AuthButton>
+            <AuthButton @click="onSignin('linear')" :disabled="opening">
+                <Icon v-if="opening" name="sync" class="animate-spin text-gray-800"></Icon>
+                <Icon v-else :size="24" name="linear"></Icon>
+                <span>{{ t("buttonText", { provider: "linear" }) }}</span>
+            </AuthButton>
+        </div>
     </div>
-  </div>
 </template>
