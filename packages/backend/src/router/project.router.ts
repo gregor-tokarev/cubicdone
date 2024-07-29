14626df97e0c62e17b0a035b2d39b7d7e420c85b
gregortokarev <gregortokarev@gregortokarevs-MacBook-Pro.local>
2024-07-29 16:24:38 +0300

    transalte landing

--- packages/backend/src/router/project.router.ts
+++ packages/backend/src/router/project.router.ts
@@ -7,58 +7,58 @@ import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 export const projects = router({
-  getAll: authedProcedure.query((opts) => {
-    return db
-      .select()
-      .from(projectTable)
-      .where(eq(projectTable.authorId, opts.ctx.user.id))
-      .execute();
-  }),
-  delete: authedProcedure.input(z.string()).mutation(async (opts) => {
-    const userId = opts.ctx.user.id;
-    const targetId = opts.input;
-
-    const [draft] = await db
-      .select()
-      .from(projectTable)
-      .where(eq(projectTable.id, targetId))
-      .execute();
-    if (!draft) throw new TRPCError({ code: "NOT_FOUND" });
-
-    if (draft.authorId !== userId)
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-
-    await db.delete(projectTable).where(eq(projectTable.id, targetId));
-
-    return "done";
-  }),
-  create: authedProcedure.input(projectTableValidator).mutation((opts) => {
-    const userId = opts.ctx.user.id;
-    return db
-      .insert(projectTable)
-      .values({ ...opts.input, authorId: userId })
-      .execute();
-  }),
-  update: authedProcedure
-    .input(projectTableValidator)
-    .mutation(async (opts) => {
-      const userId = opts.ctx.user.id;
-      const targetId = opts.input.id;
-
-      const [project] = await db
-        .select()
-        .from(projectTable)
-        .where(eq(projectTable.id, targetId))
-        .execute();
-      if (!project) throw new TRPCError({ code: "NOT_FOUND" });
-
-      if (project.authorId !== userId)
-        throw new TRPCError({ code: "UNAUTHORIZED" });
-
-      return db
-        .update(projectTable)
-        .set(opts.input)
-        .where(eq(projectTable.id, targetId))
-        .execute();
+    getAll: authedProcedure.query((opts) => {
+        return db
+            .select()
+            .from(projectTable)
+            .where(eq(projectTable.authorId, opts.ctx.user.id))
+            .execute();
     }),
+    delete: authedProcedure.input(z.string()).mutation(async (opts) => {
+        const userId = opts.ctx.user.id;
+        const targetId = opts.input;
+
+        const [draft] = await db
+            .select()
+            .from(projectTable)
+            .where(eq(projectTable.id, targetId))
+            .execute();
+        if (!draft) throw new TRPCError({ code: "NOT_FOUND" });
+
+        if (draft.authorId !== userId)
+            throw new TRPCError({ code: "UNAUTHORIZED" });
+
+        await db.delete(projectTable).where(eq(projectTable.id, targetId));
+
+        return "done";
+    }),
+    create: authedProcedure.input(projectTableValidator).mutation((opts) => {
+        const userId = opts.ctx.user.id;
+        return db
+            .insert(projectTable)
+            .values({ ...opts.input, authorId: userId })
+            .execute();
+    }),
+    update: authedProcedure
+        .input(projectTableValidator)
+        .mutation(async (opts) => {
+            const userId = opts.ctx.user.id;
+            const targetId = opts.input.id;
+
+            const [project] = await db
+                .select()
+                .from(projectTable)
+                .where(eq(projectTable.id, targetId))
+                .execute();
+            if (!project) throw new TRPCError({ code: "NOT_FOUND" });
+
+            if (project.authorId !== userId)
+                throw new TRPCError({ code: "UNAUTHORIZED" });
+
+            return db
+                .update(projectTable)
+                .set(opts.input)
+                .where(eq(projectTable.id, targetId))
+                .execute();
+        }),
 });
---
+++ packages/landing/src/i18n/ui.ts
@@ -0,0 +1,36 @@
+export const languages = {
+    en: 'English',
+    ru: 'Русский',
+};
+
+export const defaultLang = 'ru';
+
+export const ui = {
+    en: {
+        'nav.pricing': 'Pricing',
+        'nav.signup': 'Sign up',
+        'nav.login': 'Log in',
+
+        'hero.title': `Do not spend days to create
+      <span class="animated-gradient-text">productivity system</span>, we already do that`,
+        'hero.subtitle': "TODO List with preconfigured productivity system, that will keep your tasks and mind in shape",
+        'hero.action': "Try it now!",
+
+        'footer.subtitle': "TODO List with preconfigured productivity system, that will keep your tasks and mind in shape",
+
+        'askquestion': "Ask a question"
+    },
+    ru: {
+        'nav.pricing': 'Стоимость',
+        'nav.signup': 'Регистрация',
+        'nav.login': 'Вход',
+
+        'hero.title': `Не тратьте дни на создание <span class="animated-gradient-text">Системы продуктивности</span>, мы уже сделали это за вас`,
+        'hero.subtitle': "Список дел с подготовленной системой продуктивности, которая сохранит ваши задачи и мышление в тонусе",
+        'hero.action': "Пробовать сейчас!",
+
+        'footer.subtitle': "Список дел с подготовленной системой продуктивности, которая сохранит ваши задачи и мышление в тонусе",
+
+        'askquestion': "Задать вопрос"
+    },
+} as const;
