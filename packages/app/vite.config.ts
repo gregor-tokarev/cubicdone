5b4b52f69410d78f4a44b4e0a4448b3e77880029
gregortokarev <gregortokarev@gregortokarevs-MacBook-Pro.local>
2024-07-31 12:28:21 +0300

    add server to vite config

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -7,39 +7,48 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    createSVGSpritePlugin({
-      svgFolder: "./src/assets/svg",
-      transformIndexHtmlTag: {
-        injectTo: "body",
-      },
-    }),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
-    }),
-    sentryVitePlugin({
-      org: "me-sgp",
-      project: "cubicdone",
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: "@components",
-        replacement: path.resolve(__dirname, "./src/components"),
-      },
-      { find: "@store", replacement: path.resolve(__dirname, "./src/store") },
-      { find: "@models", replacement: path.resolve(__dirname, "./src/models") },
-      { find: "@utils", replacement: path.resolve(__dirname, "./src/utils") },
-      { find: "@assets", replacement: path.resolve(__dirname, "./src/assets") },
+    plugins: [
+        vue(),
+        createSVGSpritePlugin({
+            svgFolder: "./src/assets/svg",
+            transformIndexHtmlTag: {
+                injectTo: "body",
+            },
+        }),
+        VitePWA({
+            registerType: "autoUpdate",
+            injectRegister: "auto",
+            workbox: { globPatterns: ["**/*.{js,css,html,ico,png,svg}"] },
+        }),
+        sentryVitePlugin({
+            org: "me-sgp",
+            project: "cubicdone",
+        }),
     ],
-  },
+    resolve: {
+        alias: [
+            {
+                find: "@components",
+                replacement: path.resolve(__dirname, "./src/components"),
+            },
+            { find: "@store", replacement: path.resolve(__dirname, "./src/store") },
+            { find: "@models", replacement: path.resolve(__dirname, "./src/models") },
+            { find: "@utils", replacement: path.resolve(__dirname, "./src/utils") },
+            { find: "@assets", replacement: path.resolve(__dirname, "./src/assets") },
+        ],
+    },
 
-  build: {
-    target: "esnext",
-    sourcemap: true,
-  },
+    build: {
+        target: "esnext",
+        sourcemap: true,
+    },
+
+    server: {
+        port: 3000,
+        host: true,
+    },
+    preview: {
+        port: 3000,
+        host: true,
+    },
 });
