96b22f39c08683150f0d298c5d3bc0e7e2ecb066
gregortokarev <gregortokarev@gregortokarevs-MacBook-Pro.local>
2024-08-05 14:40:21 +0300

    add flows-js

--- packages/app/src/views/layout/office.vue
+++ packages/app/src/views/layout/office.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DeleteModal from "@components/DeleteModal.vue";
+import * as flows from "@flows/js"
 import ProjectModal from "@components/SelectModals/ProjectModal.vue";
 import Sidebar from "@components/Sidebar.vue";
 import { useDraftsStore } from "@store/drafts.ts";
@@ -22,71 +23,74 @@ const projectStatusStore = useProjectStatusStore();
 
 const synced = ref(true);
 onMounted(async () => {
-  hotkeys.filter = (event) => {
-    if (event.metaKey) return true;
+    flows.init({ projectId: "25afae2c-fee0-4023-af9e-9e1c62d96b0c" })
 
-    const target = event.target as HTMLElement;
-    const tagName = target?.tagName;
+    hotkeys.filter = (event) => {
+        if (event.metaKey) return true;
 
-    return !(
-      target.isContentEditable ||
-      tagName == "INPUT" ||
-      tagName == "SELECT" ||
-      tagName == "TEXTAREA"
-    );
-  };
+        const target = event.target as HTMLElement;
+        const tagName = target?.tagName;
 
-  // go to inbox page hotkey
-  hotkeys("cmd+i", (evt) => {
-    evt.preventDefault();
-    router.push("/inbox");
-  });
+        return !(
+            target.isContentEditable ||
+            tagName == "INPUT" ||
+            tagName == "SELECT" ||
+            tagName == "TEXTAREA"
+        );
+    };
 
-  // go to plan page hotkey
-  hotkeys("cmd+g", (evt) => {
-    evt.preventDefault();
-    router.push("/");
-  });
+    // go to inbox page hotkey
+    hotkeys("cmd+i", (evt) => {
+        evt.preventDefault();
+        router.push("/inbox");
+    });
 
-  hotkeys("cmd+p", (evt) => {
-    evt.preventDefault();
-    router.push("/projects");
-  });
+    // go to plan page hotkey
+    hotkeys("cmd+g", (evt) => {
+        evt.preventDefault();
+        router.push("/");
+    });
 
-  hotkeys("cmd+o", (evt) => {
-    evt.preventDefault();
-    router.push("/profile");
-  });
+    hotkeys("cmd+p", (evt) => {
+        evt.preventDefault();
+        router.push("/projects");
+    });
 
-  try {
-    await Promise.all([
-      integrationStore.backwardSync(),
-      taskStore.backwardSync(),
-      projectStore.backwardSync(),
-      draftStore.backwardSync(),
-      projectStatusStore.backwardSync(),
-    ]);
-    synced.value = true;
-  } catch (e) {
-    console.log(e);
-  }
+    hotkeys("cmd+o", (evt) => {
+        evt.preventDefault();
+        router.push("/profile");
+    });
+
+    try {
+        await Promise.all([
+            integrationStore.backwardSync(),
+            taskStore.backwardSync(),
+            projectStore.backwardSync(),
+            draftStore.backwardSync(),
+            projectStatusStore.backwardSync(),
+        ]);
+        synced.value = true;
+    } catch (e) {
+        console.log(e);
+    }
 });
 </script>
 
 <template>
-  <div v-if="synced" class="flex h-[100vh] items-start">
-    <Sidebar class="h-full"></Sidebar>
-    <div class="relative h-full grow overflow-y-auto" data-scroll-container>
-      <div class="relative mx-auto h-full max-w-[980px]" data-office-wrapper>
-        <router-view></router-view>
-      </div>
+    <div v-if="synced" class="flex h-[100vh] items-start">
+        <Sidebar class="h-full"></Sidebar>
+        <div class="relative h-full grow overflow-y-auto" data-scroll-container>
+            <div class="relative mx-auto h-full max-w-[980px]" data-office-wrapper>
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
-  </div>
-  <div v-else class="fixed inset-0 flex items-center justify-center bg-white">
-    <VueSpinnerPuff size="200px" color="#333"></VueSpinnerPuff>
-  </div>
-  <ProjectModal></ProjectModal>
-  <DeleteModal></DeleteModal>
+    <div v-else class="fixed inset-0 flex items-center justify-center bg-white">
+        <VueSpinnerPuff size="200px" color="#333"></VueSpinnerPuff>
+    </div>
+    <ProjectModal></ProjectModal>
+    <DeleteModal></DeleteModal>
 </template>
+
 
 <style scoped></style>
