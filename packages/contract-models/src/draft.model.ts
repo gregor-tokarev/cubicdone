8f29a8bfe1442af3cc77e8b60f331eba6e2bd92a
gregortokarev <gregortokarev@gregortokarevs-MacBook-Pro.local>
2024-10-17 14:04:51 +0300

    update

--- packages/contract-models/src/draft.model.ts
+++ packages/contract-models/src/draft.model.ts
@@ -1,15 +1,16 @@
+
 export interface Draft {
-  id: string;
-  title: string;
-  dateCreated: string;
-  dateUpdated: string;
-  order: number;
-  projectId: string | null;
-  external?: {
-    integrationName: string;
-    projectTitle?: string;
-    projectId?: string;
-    link: string;
-    iconURL: string;
-  };
+    id: string;
+    title: string;
+    dateCreated: string;
+    dateUpdated: string;
+    order: number;
+    projectId: string | null;
+    external?: {
+        integrationName: string;
+        projectTitle?: string;
+        projectId?: string;
+        link: string;
+        iconURL: string;
+    };
 }
